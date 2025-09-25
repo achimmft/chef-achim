5c64a730b3a177e1070e0b0daea8251b39568d89
achimmft <70777695+achimmft@users.noreply.github.com>
2025-09-25 11:55:21 -0600

    Project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import logo from "../assets/chef-achim-icon.png"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={logo} alt="logo" />
+            <h1>Chef Achim</h1>
+        </header>
+    )
+}
+// <header>
+//     <img src={logo} alt="logo" />
+//     <h1>Chef Achim</h1>
+// </header>
