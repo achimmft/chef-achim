b2e375f89ba8e7bd4a52254f67c7d4363bf6e1a0
achimmft <70777695+achimmft@users.noreply.github.com>
2025-09-25 16:26:25 -0600

    updating states

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,30 @@
+import React from "react";
+
 export default function Main() {
+  const [ingredients, setIngridients] = React.useState([]);
+  const ingredientListITems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngridients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g oregano"
           aria-label="add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingredients.length > 0 && ingredientListITems}</ul>
     </main>
   );
 }
